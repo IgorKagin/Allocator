ada9fb74b5d7553f1f633371ace8fdcfab7bfc29
IgorKagin <60917726+IgorKagin@users.noreply.github.com>
2023-08-11 12:41:58 +0300

    Create vector.h

---
+++ include/vector.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "allocator.h"
+#include <memory>
+
+namespace VectorRealize 
+{
+  // Реализация класса вектора
+  template<typename T, class Allocator = std::allocator<T>>
+  class Vector final
+  {
+  private:
+
+    T * arr = nullptr;    //  указатель на массив
+    size_t sz = 0;        //  количество элементов
+    size_t cap = 0;       //  количество выделенной памяти
+    Allocator alloc;      //  аллокатор
+  
+    using AllocTraits = std::allocator_traits<Allocator>;
+
+  public:
+
+    Vector();
+
+    ~Vector();
+
+    void reserve( size_t n );  
+
+    void push_back( const T& value );
+
+    void pop_back();
+
+    T& operator[]( size_t );
+
+    T& at( size_t t ); 
+
+  };
+}
+
+#include "vector.hpp"
